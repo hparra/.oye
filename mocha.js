b9eb6b78bb178921c84f5bbab302b83aa4f0f3fa
HGPA <hector@hectorparra.com>
2016-09-05 14:48:22 -0700

    improve mocha oye

--- mocha.js
+++ mocha.js
@@ -1,9 +1,10 @@
 
 const FS = require('fs')
-const Assert = require('assert')
+const Assert = require('assert') // https://nodejs.org/api/assert.html
 
 describe('Test Famlily', () => {
   it('should test', (done) => {
     // remember you can return Promises
+    Assert.equal(false, true) // (actual, expected[, message])
   })
 })
